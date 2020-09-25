a0cadbe73da0e850b3977b2e59eb8406a8f41d46
Radovan Bast <bast@users.noreply.github.com>
2020-09-25 19:02:24 +0200

    add columns where A1 is subtracted

--- process.py
+++ process.py
@@ -76,22 +76,39 @@ def std_dev(numbers):
 def read_data(file_name):
     with open(file_name, "r") as f:
         data_590_nm = []
+        data_590_nm_minus_blank = []
         data_750_nm = []
+        data_750_nm_minus_blank = []
 
         # this is done this way to allow for more than 3 plates, it could be in
         # fact any number of plates
         try:
             while True:
                 data_590_nm_plate, data_750_nm_plate = read_chunk(f)
+
                 data_590_nm.append(data_590_nm_plate)
+                data_590_nm_minus_blank.append(
+                    [x - data_590_nm_plate[0] for x in data_590_nm_plate]
+                )
                 data_750_nm.append(data_750_nm_plate)
+                data_750_nm_minus_blank.append(
+                    [x - data_750_nm_plate[0] for x in data_750_nm_plate]
+                )
+
         except StopIteration:
             pass
 
         data_590_nm_tuples = zip_to_list_of_tuples(data_590_nm)
+        data_590_nm_minus_blank_tuples = zip_to_list_of_tuples(data_590_nm_minus_blank)
         data_750_nm_tuples = zip_to_list_of_tuples(data_750_nm)
+        data_750_nm_minus_blank_tuples = zip_to_list_of_tuples(data_750_nm_minus_blank)
 
-        return data_590_nm_tuples, data_750_nm_tuples
+        return (
+            data_590_nm_tuples,
+            data_590_nm_minus_blank_tuples,
+            data_750_nm_tuples,
+            data_750_nm_minus_blank_tuples,
+        )
 
 
 def compute_statistics(tuples):
@@ -123,16 +140,43 @@ def print_output(*args):
         print(",".join(map(str, row)))
 
 
+def round_numbers(lists):
+    num_digits = 3
+    for xs in lists:
+        for i in range(len(xs)):
+            xs[i] = round(xs[i], num_digits)
+
+
 if __name__ == "__main__":
     args = parse_command_line()
 
-    data_590_nm_tuples, data_750_nm_tuples = read_data(args.txt_file)
+    (
+        data_590_nm_tuples,
+        data_590_nm_minus_blank_tuples,
+        data_750_nm_tuples,
+        data_750_nm_minus_blank_tuples,
+    ) = read_data(args.txt_file)
 
     averages_590_nm, std_devs_590_nm = compute_statistics(data_590_nm_tuples)
-    averages_750_nm, std_devs_750_nm = compute_statistics(data_750_nm_tuples)
-
     lns_590_nm = list(map(math.log, averages_590_nm))
+    averages_590_nm_minus_blank, std_devs_590_nm_minus_blank = compute_statistics(
+        data_590_nm_minus_blank_tuples
+    )
+
+    averages_750_nm, std_devs_750_nm = compute_statistics(data_750_nm_tuples)
     lns_750_nm = list(map(math.log, averages_750_nm))
+    averages_750_nm_minus_blank, std_devs_750_nm_minus_blank = compute_statistics(
+        data_750_nm_minus_blank_tuples
+    )
+
+    ratio1 = [a / b for (a, b) in zip(averages_590_nm, averages_750_nm)]
+    # we skip first term because division by zero
+    ratio2 = [
+        a / b
+        for (a, b) in zip(
+            averages_590_nm_minus_blank[1:], averages_750_nm_minus_blank[1:]
+        )
+    ]
 
     labels, sources = read_wells(args.wells)
 
@@ -141,22 +185,48 @@ if __name__ == "__main__":
     hours = [extract_hours_from_filename(args.txt_file)] * len(sources)
 
     # we round everything to 3 digits after the comma
-    num_digits = 3
-    averages_590_nm = list(map(lambda x: round(x, num_digits), averages_590_nm))
-    std_devs_590_nm = list(map(lambda x: round(x, num_digits), std_devs_590_nm))
-    lns_590_nm = list(map(lambda x: round(x, num_digits), lns_590_nm))
-    averages_750_nm = list(map(lambda x: round(x, num_digits), averages_750_nm))
-    std_devs_750_nm = list(map(lambda x: round(x, num_digits), std_devs_750_nm))
-    lns_750_nm = list(map(lambda x: round(x, num_digits), lns_750_nm))
+    round_numbers(
+        [
+            averages_590_nm,
+            averages_590_nm_minus_blank,
+            std_devs_590_nm,
+            std_devs_590_nm_minus_blank,
+            lns_590_nm,
+            averages_750_nm,
+            averages_750_nm_minus_blank,
+            std_devs_750_nm,
+            std_devs_750_nm_minus_blank,
+            lns_750_nm,
+            ratio1,
+            ratio2,
+        ]
+    )
+
+    # since ratio1 and ratio2 are missing the first element (division by zero),
+    # we need to add it:
+    ratio1 = ["-"] + ratio1
+    ratio2 = ["-"] + ratio2
+
+    print(
+        "labels,sources,hours,averages_590_nm,std_devs_590_nm,averages_590_nm_minus_blank,std_devs_590_nm,std_devs_590_nm_minus_blank,lns_590_nm,averages_750_nm,std_devs_750_nm,averages_750_nm_minus_blank,std_devs_750_nm,std_devs_750_nm_minus_blank,lns_750_nm,ratio1,ratio2"
+    )
 
     print_output(
         labels,
         sources,
         hours,
-        averages_750_nm,
-        std_devs_750_nm,
-        lns_750_nm,
         averages_590_nm,
         std_devs_590_nm,
+        averages_590_nm_minus_blank,
+        std_devs_590_nm,
+        std_devs_590_nm_minus_blank,
         lns_590_nm,
+        averages_750_nm,
+        std_devs_750_nm,
+        averages_750_nm_minus_blank,
+        std_devs_750_nm,
+        std_devs_750_nm_minus_blank,
+        lns_750_nm,
+        ratio1,
+        ratio2,
     )
